d963946f7526ac10de1afa2d7ab901cf08d11a90
Nickolas J. Petrone <nickolasjpetrone@outlook.com>
2024-11-08 09:51:17 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SortingProj
+{
+    internal class Program
+    {
+        static void InsertionSort(string[] args)
+        {
+            int[] arr = { 4, 5, 1, 3, 2, 6, 0, 8 };
+            int curr, i, j;
+            for (i = 1; i < arr.Length; i++)
+            {
+                curr = arr[i];
+                for (j = i - 1; j >= 0 && arr[j] > curr; j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+                arr[j + 1] = curr;
+            }
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProj
+{
+    internal class Sort
+    {
+        static void Main()
+        {
+            int[] a = { 2, 3, 7, 8 };
+            int[] b = { 0, 1, 9, 10 };
+            int[] c = new int[8];
+            MergeArray(a, b, c);
+        }
+
+        static void MergeArray(int[] a, int[] b, int[] c)
+        {
+            int i = 0, j = 0, k = 0;
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c[k++] = a[i++];
+                }
+                else
+                {
+                    c[k++] = b[j++];
+                }
+            }
+            while (i < a.Length)
+            {
+                c[k++] = a[i++];
+            }
+            while (j < b.Length)
+            {
+                c[k++] = b[j++];
+            }
+            for (int l = 0; l < c.Length; l++)
+            {
+                Console.WriteLine(c[l]);
+            }
+        }
+    }
+}
